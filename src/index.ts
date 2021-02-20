03dcc7750be79b95080da003ce6165bc419da0a0
Michael Arnaldi <arnaldimichael@gmail.com>
2021-02-20 14:35:57 +0000

    prove fine

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { pipe } from "@effect-ts/core/Function";
 import { getA, LiveLayerA } from "./layer-a";
 import * as R from "@effect-ts/node/Runtime";
 import * as T from "@effect-ts/core/Effect";
+import * as L from "@effect-ts/core/Effect/Layer";
 
 pipe(
   getA,
   T.tap((a) => T.effectTotal(() => console.log(a))),
-  T.provideSomeLayer(LiveLayerA),
+  T.provideSomeLayer(LiveLayerA["+++"](L.Empty)),
   T.result,
   T.chain(T.done),
   R.runMain
